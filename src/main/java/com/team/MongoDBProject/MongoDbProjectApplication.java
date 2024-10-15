f740cf6194e864840d3eac3015ef5b671267ea38
yohanvillarp <yohannikel21@gmail.com>
2024-10-14 23:08:43 -0500

    iniciando proyecto

---
+++ src/main/java/com/team/MongoDBProject/MongoDbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.team.MongoDBProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/MongoDBProject/MongoDbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.MongoDBProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
